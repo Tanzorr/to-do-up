237868f41e12ca0fe440df10e1f07466c886ff04
tanzorr <alexx1984@ukr.net>
2023-08-01 12:54:13 +0300

    first commit

---
+++ src/app/libs/task-list-table/task-list-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-table',
+  templateUrl: './task-list-table.component.html',
+  styleUrls: ['./task-list-table.component.css']
+})
+export class TaskListTableComponent {
+
+}
---
+++ src/app/libs/task-list-table/task-list-table.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskListTableComponent } from './task-list-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    TaskListTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TaskListTableModule { }
